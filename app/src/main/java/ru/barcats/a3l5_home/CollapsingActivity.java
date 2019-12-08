576cad97be9917a9f451dee708cf722b72dfefb1
Андрей <barcats@inbox.ru>
2019-12-08 21:48:12 +0300

    сделал CollapsingActivity

--- app/src/main/java/ru/barcats/a3l5_home/CollapsingActivity.java
+++ app/src/main/java/ru/barcats/a3l5_home/CollapsingActivity.java
@@ -1,14 +1,20 @@
 package ru.barcats.a3l5_home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class CollapsingActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collapsing);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
---
+++ app/src/main/java/ru/barcats/a3l5_home/FabActivity.java
@@ -0,0 +1,14 @@
+package ru.barcats.a3l5_home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FabActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fab);
+    }
+}
