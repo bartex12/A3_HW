77f93858f3d4eea347c0dffefa003f94f3a92556
Андрей <barcats@inbox.ru>
2019-12-18 21:00:14 +0300

    сделал задание 1 - 5 кнопок в своём стиле

---
+++ app/src/main/java/ru/barcats/a3l5_home/ThemeActivity.java
@@ -0,0 +1,14 @@
+package ru.barcats.a3l5_home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThemeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_theme);
+    }
+}
