50bad2313dc8128894109440674633db5385fe17
Андрей <barcats@inbox.ru>
2019-12-18 21:40:58 +0300

    сделал задание 2 - зелёная тема с FAB

--- app/src/main/java/ru/barcats/a3l5_home/ThemeActivity.java
+++ app/src/main/java/ru/barcats/a3l5_home/ThemeActivity.java
@@ -3,12 +3,27 @@ package ru.barcats.a3l5_home;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class ThemeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.AppThemeGreen);
         setContentView(R.layout.activity_theme);
+
+        FloatingActionButton fab = findViewById(R.id.fabTheme);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ThemeActivity.this,
+                        getResources().getString(R.string.simple_button),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
